64c193ee2b3923e66896166396db005a212a29bd
Ronaldo Winter Junior <>
2015-11-18 22:52:00 -0200

    Alterações (validação) http://www.w3schools.com/php/php_forms.asp

--- index.php
+++ index.php
@@ -1,21 +1,87 @@
+<?php
+// define variables and set to empty values
+$nameErr = $emailErr = $genderErr = $websiteErr = "";
+$name = $email = $gender = $comment = $website = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (empty($_POST["name"])) {
+        $nameErr = "Name is required";
+    } else {
+        $name = test_input($_POST["name"]);
+        if (!preg_match("/^[a-zA-Z ]*$/", $name)) {
+            $nameErr = "Apenas letras e espaçoes em branco.";
+        }
+    }
+
+    if (empty($_POST["email"])) {
+        $emailErr = "Email is required";
+    } else {
+        $email = test_input($_POST["email"]);
+    }
+
+    if (empty($_POST["website"])) {
+        $website = "";
+    } else {
+        $website = test_input($_POST["website"]);
+    }
+
+    if (empty($_POST["comment"])) {
+        $comment = "";
+    } else {
+        $comment = test_input($_POST["comment"]);
+    }
+
+    if (empty($_POST["gender"])) {
+        $genderErr = "Gender is required";
+    } else {
+        $gender = test_input($_POST["gender"]);
+    }
+}
+
+function test_input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
+?>
 <!DOCTYPE html>
 <html>
     <head>
         <meta charset="UTF-8">
         <title></title>
+        <style>
+            .err{
+                ouline: 1px dashed red;
+                background-color: blanchedalmond;
+            }
+        </style>
     </head>
     <body>
-        
+
         <h1> Manuseio de Formulário com PHP </h1>
         <p>Referrências:</p>
         <ul>
             <li><a href="http://www.w3schools.com/php/">W3 Schools</a></li>
             <li><a href="http://php.net/">Manual do PHP</a></li>
         </ul>
-        
-        <?php
-     
-          
-        ?>
-    </body>
-</html>
+        <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+            Name: <input type="text" name="name" class="<?= strlen($nameErr) != 0 ? "err" : ""; ?>">
+            <span class="error">* <?php echo @$nameErr; ?></span>
+            <br><br>
+            E-mail:
+            <input type="text" name="email" class="<?= strlen($emailErr) != 0 ? "err" : ""; ?>">
+            <span class="error">* <?php echo @$emailErr; ?></span>
+            <br><br>
+            Comment: <textarea name="comment" rows="5" cols="40"></textarea>
+            <br><br>
+            Gender:
+            <input type="radio" name="gender" value="female">Female
+            <input type="radio" name="gender" value="male">Male
+            <span class="error">* <?php echo @$genderErr; ?></span>
+            <br><br>
+            <input type="submit" name="submit" value="Submit">
+
+
+            </body>
+            </html>
